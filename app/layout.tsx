653af3b747bd428dd32c053da79821f6d2b3adb5
joaqo-dev <joaquincm30@gmail.com>
2026-02-12 07:18:11 -0300

    Fix mobile safe area white spaces on iOS devices

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,6 +13,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="es">
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
+        <meta name="theme-color" content="#000000" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+      </head>
       <body>{children}</body>
     </html>
   );
